6863cc5aa7cfab00eb87ff40302ab50d5fc1e05f
petargenev <petar.genev.mail@gmail.com>
2016-10-06 18:53:11 +0300

    deleting old files

--- WebContent/js/PasswordMatcher.js
+++
@@ -1,9 +0,0 @@
-'use strict;'
-function check(input) {
-	if (input.value != document.getElementById('userPsw').value) {
-		input.setCustomValidity('Двете пароли не съвпадат!');
-	} else {
-		// input is valid -- reset the error message
-		input.setCustomValidity('');
-	}
-}
